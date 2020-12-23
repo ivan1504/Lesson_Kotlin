1ed004830e10934f1bde947f42204ce99eaa7579
Ivan Chubutkin <ivanchubutkin@rambler.ru>
2020-12-23 23:18:51 +0300

    refactoring

--- semafor/src/main/java/com/example/semafor/MainActivity.kt
+++ semafor/src/main/java/com/example/semafor/MainActivity.kt
@@ -4,11 +4,17 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
+import java.util.*
 
 class MainActivity : Activity() {
 
     var imSemafor:ImageView?=null
+    var counter:Int=0
+    var timer:Timer?=null
+    var is_run=false
+    var imageArray:IntArray= intArrayOf(R.drawable.semafor_red,R.drawable.semafor_yellow,R.drawable.semafor_green)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +23,37 @@ class MainActivity : Activity() {
         imSemafor=findViewById(R.id.imSemafor)
     }
 
-    fun onClickStartStop(view: View) {}
+    fun onClickStartStop(view: View) {
+
+        view as ImageButton
+        if (!is_run){
+            startStop()
+            view.setImageResource(R.drawable.button_stop)
+            is_run=true
+        }else{
+            imSemafor?.setImageResource(R.drawable.semafor_grey)
+            view.setImageResource(R.drawable.button_start)
+            timer?.cancel()
+            is_run=false
+            counter=0
+
+        }
+
+    }
+
+    fun startStop(){
+        timer = Timer()
+        timer?.schedule(object :TimerTask(){
+            override fun run() {
+                runOnUiThread{
+                    imSemafor?.setImageResource(imageArray[counter])
+                    counter++
+                    if (counter ==3)counter=0
+                }
+
+            }
+
+        },0,2000)
+    }
 
 }
